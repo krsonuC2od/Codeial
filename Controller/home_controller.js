4a4ce429a4e23dc92db40fea3bc4a4f58ce5efc6
krsonuC2od <190101120062@cutm.ac.in>
2023-04-30 10:03:16 +0530

    POST :: Display post and related User

--- Controller/home_controller.js
+++ Controller/home_controller.js
@@ -1,13 +1,24 @@
-
+const Post = require('../Models/post');
+const { post } = require('../routes/post');
 // step 2 part of controller create controller of home page
 module.exports.home = function (req, res) {
-  console.log(req.cookies);
+  // console.log(req.cookies);
   //  return res.end('<h1>Controller is working</h1>')
-  return res.render("home", {
-    title: "Codeial",
+  // Post.find({},function(err,posts){
+  //   return res.render("home", {
+  //     title: "Codeial | Home",
+  //     posts:posts
+  //   });
+  // });
+
+  Post.find({}).populate('user').exec(function(err,posts){
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts:posts
+    });
   });
-};
+}
 
 
 
-module.exports.createSession = function (req, res) {};
+// module.exports.createSession = function (req, res) {};
