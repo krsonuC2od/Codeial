33c59a470e75c6bb4c80d7a3a4c6cb9644e33c8b
krsonuC2od <190101120062@cutm.ac.in>
2023-05-19 11:47:04 +0530

    Sending Enail :: sending notification to user by throw realtime email

--- Controller/comment_controller.js
+++ Controller/comment_controller.js
@@ -1,26 +1,44 @@
 const Comment = require('../Models/comment');
 const Post = require('../Models/post');
+const commentMailer = require('../Controller/mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req,res){
    try{ 
   let post = await Post.findById(req.body.post);
-        
+      //   console.log(post,"*****");
+
         if(post){
             
          let comment = await Comment.create({
+
              
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id
                 
             });
+            // console.log(comment,"***Comment****");
                 post.comments.push(comment);
                 post.save();
+               
+                comment = await comment.populate('user','name email');
+               
+               // commentMailer.newComment(comment);  
+               let job = queue.create('emails',comment).save(function(err){
+                  if(err){
+                     console.log('error in creating a queue ',err);
+                     return;
+                  }
+                  console.log('job enqueue',job.id);
+               }) 
       req.flash('success','comment added on post');
                 res.redirect('/');
           
         }
     }catch(err){
+      console.log('calling from error');
        req.flash('error',err);
        res.redirect('/');
     }
---
+++ config/nodemailer.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+
+let transporter =nodemailer.createTransport({
+    service: 'gmail',
+    host:'smtp.gmail.com',
+    port: 587,
+    secure: false,
+     auth: {
+        user: 'krsonu923@gmail.com',
+        pass: 'uuhlooqkedgshouz'
+        
+     }
+});
+
+let renderTemplate = (data, relativePath) =>    {
+    let mailHtml;
+     ejs.renderFile(
+        path.join(__dirname, '../views/mailers',relativePath),
+        data,
+       
+        function(err ,template){
+            if(err){
+                console.log('error in rendering template ',err);
+                return;
+            }
+            mailHtml = template;
+        }
+     )
+     return mailHtml;
+}
+
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
